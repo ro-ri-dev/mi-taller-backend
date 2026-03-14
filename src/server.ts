2394c10d0150e58b9201a83ea559675e7ecdb0dc
Rocio Ricciardiello <paragit@gmail.com>
2026-03-14 18:34:54 +0100

    feat: backend base structure with router

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
-import express from 'express'
-
-const app = express()
+import app from './app.js'
 
 const PORT = 3000
-
-app.get('/', (req, res) => {
-  res.send('Mi Taller API')
-})
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`)
